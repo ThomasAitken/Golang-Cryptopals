ab6b05d38225c34cec4bf9021acb24b3ad129c04
Tom Aitken <tom@shop-grok.com>
2020-03-26 18:37:43 +1100

    Made 3 general so as to make 4 simple

--- cryptopals_basics.go
+++ cryptopals_basics.go
@@ -21,6 +21,11 @@ import (
 	// "log"
 )
 
+type decipheredData struct {
+	Candidate []byte
+	Score float64
+}
+
 func main() {
 	var challengeNumber string = os.Args[1]
 	var input string = os.Args[2]
@@ -32,8 +37,11 @@ func main() {
 		var output string = fixedXOR(input, secondInput)
 		fmt.Println(output)
 	} else if challengeNumber == "3" {
-		var x string = decodeXORCipher(input)
-		fmt.Println(x)
+		solution := decodeXORCipher(input)
+		fmt.Println(string(solution.Candidate))
+		fmt.Println(solution.Score)
+	} else if challengeNumber == "4" {
+
 	}
 	return
 }
@@ -42,7 +50,7 @@ func main() {
 func decodeHex(input string) []byte { 
 	bytes, err := hex.DecodeString(input)
 	if err != nil {
-		fmt.Printf("failed to decode hex: %s", err)
+		return nil
 	}
 	return bytes
 }
@@ -68,9 +76,13 @@ func fixedXOR(firstInput string, secondInput string) string {
 	return hexOut
 } 
 
-//helper for 3,4
+//helper for 3,4.. creates map from ASCII bytes to their frequency (as a proportion)
+//takes as input not a byte slice but a string, either hex or standard string
 func getFrequencies(input string) map[byte]float64 { 
 	bytes := decodeHex(input)
+	if bytes == nil { 
+		bytes = []byte(input)
+	}
 	frequenciesMap := make(map[byte]float64)
 	var totalBytes int = len(bytes)
 	for _, asciiByte := range bytes { 
@@ -114,24 +126,38 @@ func sortByFrequencies(input map[byte]float64) [][]float64 {
 	return hackkeys
 }
 
-/*measure distance of decoded text in terms of character frequencies from average Portrait of Artist text of same length by very simple method of summing the squared differences of the percentages of each character (i.e. for char 'e', it might be (10-15)^2). Those characters that exist in one text and not the other are simply ignored, under the assumption that they will not affect the results in any important way.
+/*measure distance of decoded text in terms of character frequencies from average Portrait of Artist text of same length by very simple method of summing the squared differences of the percentages of each character (i.e. for char 'e', it might be (10-15)^2). 
 Since we are measuring distance from a representative probability distribution, the smaller the score the better.
 */ 
-func scoreDecodedText(inputLength int, decodedFrequencies map[byte]float64, languageData map[byte]float64) float64 { 
+func scoreDecipheredText(decodedFrequencies map[byte]float64, languageData map[byte]float64) float64 { 
 	var score float64
-	for key, val := range decodedFrequencies {
-
+	for key, val := range languageData {
+		_, exists := decodedFrequencies[key]
+		if !exists {
+			decodedFrequencies[key] = float64(0)
+		}
+		//decodedFrequencies vals are proportions not percentages so multiply by 100 before squaring diff
+		score += math.Pow(val - 100*decodedFrequencies[key], 2)
 	}
+	for key, val := range decodedFrequencies { 
+		_, exists := languageData[key]
+		//account for strange chars that appear in texts deciphered with wrong cipher. If a weird char has high frequency in 'deciphered' string, then obviously that should push up the score!
+		if !exists {
+			score += math.Pow(100*val - 0, 2)
+		}
+	}
+	// fmt.Println(score)
+	return score
 }
 
 //challenge3 main
-func decodeXORCipher(input string) string {
-	//Source: edited version of Portrait of Artist as a Young Man.. not a perfect source but lazy.. see io.py for ugly source code
-	//turns out you can literally call runes 'bytes' and the compiler doesn't complain
+func decodeXORCipher(input string) decipheredData {
+	//Source: edited version of Portrait of Artist as a Young Man.. not a perfect source but lazy and sentimental.. see io.py for ugly source code
+	//turns out you can literally call runes 'bytes' and the compiler doesn't complain = fun lifehack!
 	portraitOfArtistData := map[byte]float64{'P': 0.02, 'r': 4.49, 'o': 5.82, 'd': 3.87, 'u': 2.05, 'c': 1.75, 'e': 10.09, ' ': 17.23, 'b': 1.11, 'y': 1.41, 'C': 0.08, 'l': 3.55, 'h': 5.48, 'a': 6.29, 't': 6.85, '.': 0.94, 'H': 0.23, 'T': 0.25, 'M': 0.07, 'L': 0.04, 'v': 0.63, 's': 5.15, 'i': 5.23, 'n': 5.55, 'A': 0.15, 'F': 0.05, 'w': 1.8, 'f': 2.09, 'Y': 0.02, 'g': 1.66, 'J': 0.02, 'm': 1.82, 'p': 1.27, 'I': 0.16, 'V': 0.01, '"': 0.06, 'E': 0.03, 'O': 0.04, ',': 0.79, '1': 0.0, '8': 0.0, 'k': 0.66, ':': 0.13, 'B': 0.08, 'W': 0.07, 'q': 0.07, 'S': 0.13, '\'': 0.13, 'U': 0.01, 'D': 0.06, 'R': 0.02, 'K': 0.01, 'N': 0.03, '-': 0.05, 'x': 0.06, '!': 0.05, 'z': 0.03, 'j': 0.06, ';': 0.05, '?': 0.08, 'G': 0.07, 'Q': 0.0, '(': 0.0, ')': 0.0, '2': 0.0, '9': 0.0, 'Z': 0.0, 'X': 0.0, '3': 0.0, '0': 0.0, '4': 0.0, '5': 0.0, '6': 0.0, '7': 0.0}
 	
 	var inputFrequencies map[byte]float64 = getFrequencies(input)
-	fmt.Println(inputFrequencies)
+	// fmt.Println(inputFrequencies)
 	var topCharsInput [][]float64 = sortByFrequencies(inputFrequencies)
 	var topCharsGeneral [][]float64 = sortByFrequencies(portraitOfArtistData)
 
@@ -142,17 +168,14 @@ func decodeXORCipher(input string) string {
 		var possKey int = int(topCharsInput[0][1]) ^ int(topCharsGeneral[i][1])
 		var inputBytes []byte = decodeHex(input)
 		var candidate []byte = decipherString(inputBytes, possKey)
-		fmt.Println(string(candidate))
-		var textScore float64 = scoreDecodedText(len(inputBytes), inputFrequencies, portraitOfArtistData)
+		// fmt.Println(string(candidate))
+		var decodedFrequencies map[byte]float64 = getFrequencies(string(candidate))
+		var textScore float64 = scoreDecipheredText(decodedFrequencies, portraitOfArtistData)
 		if textScore < bestTextScore { 
 			bestTextScore = textScore
 			candidateOutput = candidate
 		}
-		if i == 4 { 
-			return candidateOutput
-		}
 	}
-	// fmt.Println(topCharsInput)
-	// fmt.Println(topCharsGeneral)
-	return ""
+	data := decipheredData{Candidate: candidateOutput, Score: bestTextScore}
+	return data 
 }
